d98e48054c3f19f41db40f511dd4034ecb144e97
Latthapon-time <62346534+Latthapon-time@users.noreply.github.com>
2020-03-19 22:20:39 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,51 +1,29 @@
-// var a = 1;
-// var b = 2;
-// function f(a,b){
-//     var c = a+b
-
-//     return c
-// }
-
-// var res = f(a,b)
-// console.log("->"+f(a,b))
-// console.log(res)
-
-
-
-
-
-// const multiply = (a,b) => {
-//     return a*b
-// }
-
-// var result = multiply(5,2)
-// console.log(result) 
-
-// function multiply1(a,b){
-
-//     return a*b
-// }
-
-// var result1 = multiply1(5,3)
-// console.log(result1)
 const obj = {
     name : 'tivakron',
-    grage : 'A',
+    grage : '2.5',
     credit : '60'
 }
 
-const funcgrede = (name = 'defaultname',grage= 'defaultgrage',credit= 'defaultcredit') => {
-if(credit<60){
-    grage = 'F'
-return ` name :${name}\n grage :${grage}\n credit :${credit}`
-}else if(credit>60){
-    grage = 'A'
-return ` name :${name}\n grage :${grage}\n credit :${credit}`
-}else{
-    return ` name :${name}\n grage :${grage}\n credit :${credit}`
-}
+const funcgrede = (name1 = 'defaultname',grage1= 'defaultgrage',credit1= 0) => {
+
+    if(credit1<60){
+        credit1 = 0
+        
+    }else{
+        credit1 = credit1
+    }
+    const obj2 = {
+        name : name1,
+        grage : grage1,
+        credit : credit1}  
+        
+        return obj2
+
+     
+   
+
 }
 
-console.log(funcgrede(obj.name,obj.grage))
-let test1 = funcgrede('time','A','50')
+
+let test1 = funcgrede('time','4.00',60)
 console.log(test1)
