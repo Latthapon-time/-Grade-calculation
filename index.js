4de506c83436150ed78a15987dc24a3101efbf39
Latthapon-time <62346534+Latthapon-time@users.noreply.github.com>
2020-03-20 00:10:54 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -8,35 +8,42 @@ const funcgrede = (name1 = 'defaultname',grage1= 0.0,credit1= 0) => {
 
     if(credit1>=60)
     {
-        if(grage1 == 0.0){
-            grage1 = 'F'
-        }else if(grage1 == 1.0){
-            grage1 = 'D'
-        }else if(grage1 == 1.5){
-            grage1 = 'D+'
-        }else if(grage1 == 2.0){
-            grage1 = 'C'
-        }else if(grage1 == 2.5){
-            grage1 = 'C+'
-        }else if(grage1 == 3.0){
-            grage1 = 'B'
-        }else if(grage1 == 3.5){
-            grage1 = 'B+'
-        }else if(grage1 == 4.0){
+        console.log('มากกว่าหรือเท่ากับ60')
+        if(grage1 > 4.0 ){
+            grage1 = 'null'
+        }
+        else if(grage1 == 4.0 ){
             grage1 = 'A'
-        }      
+        }else if(grage1 >=3.5 ){
+            grage1 = 'B+'
+        }else if(grage1 >= 3.0 ){
+            grage1 = 'B'
+        }else if(grage1 >= 2.5 ){
+            grage1 = 'C+'
+        }else if(grage1 >= 2.0 ){
+            grage1 = 'C'
+        }else if(grage1 >= 1.5 ){
+            grage1 = 'D+'
+        }else if(grage1 >= 1.0 ){
+            grage1 = 'D'
+        }else if(grage1 >= 0.0){
+            grage1 = 'F'
+        }    
     }
     else
     {
-        if(grage1 == 2.5){
-            grage1 = 'D'
-        }else if(grage1 == 3.0){
-            grage1 = 'D+'
-        }else if(grage1 == 3.5){
-            grage1 = 'C'
+        console.log('น้อยกว่า60')
+        if(grage1 > 4.0){
+            grage1 = 'null'
         }else if(grage1 == 4.0){
             grage1 = 'C+'
-        }else{
+        }else if(grage1 >= 3.5){
+            grage1 = 'C'
+        }else if(grage1 >= 3.0){
+            grage1 = 'D+'
+        }else if(grage1 >= 2.5){
+            grage1 = 'D'
+        } else{
             grage1 = 'F'
         }
     }
@@ -55,5 +62,5 @@ const funcgrede = (name1 = 'defaultname',grage1= 0.0,credit1= 0) => {
 }
 
 
-let test1 = funcgrede('time',4,50)
+let test1 = funcgrede('time',3.45,60)
 console.log(test1)
